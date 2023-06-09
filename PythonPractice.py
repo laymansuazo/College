04ed632e579265496f047a14ba7086712cb79b2c
laymansuazo <50897661+laymansuazo@users.noreply.github.com>
2023-06-08 18:45:15 -0600

    Practica

---
+++ PythonPractice.py
@@ -0,0 +1,24 @@
+print ('Hello World')
+print ("Hello World")
+print ('Good Morning,', end = ' ')
+print ('Brandon!')
+print ('Good night', 'Layman\'', 'dogs', sep=', ')
+
+
+
+print (3//2) # largest whole number, removes decimals
+print (2**3) # power
+print (7%5) # returns the remainder 
+
+def even_number(number):
+    if number % 2 == 0:
+        print('Even')
+    else:
+        print('Odd')
+even_number(50000)
+
+print (12374/621)
+print (int(12374/621))
+print (round(12374/621))
+print ('4 % 2 =', 4%2 )
+print ('4 % 2 = ' + str (4%2) )
